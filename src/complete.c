35319f016f1f738e2e4d5389f2b4980115c84924
Philippe Pepiot <philippe.pepiot@gmail.com>
2008-11-14 15:18:23 +0100

    Completion des repertoires absolus

--- src/complete.c
+++ src/complete.c
@@ -1,40 +1,70 @@
+#define _BSD_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include "complete.h"
 
+
 /* Completion sur les fichiers */
 char *file_complete(char *str)
 {
    char *p = strrchr(str, ' ');
-   char *q[100];
+   char *q[MAX_COMPLETION], *path;
    DIR *rep;
    struct dirent *file;
    size_t match = 0, i, j;
+   unsigned char type;
    if(p == NULL)
       return NULL;
    while(*p == ' ')
       p++;
    if(*p == '\0')
       return NULL;
-   rep = opendir(".");
+   /* path absolu */
+   if(*p == '/')
+   {
+      q[0] = strrchr(p, '/');
+      if(q[0] == p)
+      {
+	 path = malloc(sizeof(char) * 2);
+	 strcpy(path, "/");
+      }
+      else
+      {
+	 path = malloc(sizeof(char) * (1+q[0]-p));
+	 strncpy(path, p, q[0]-p);
+	 path[q[0]-p] = '\0';
+      }
+      p = q[0]+1;
+      rep = opendir(path);
+      free(path);
+   }
+   else
+      rep = opendir(".");
    if(rep != NULL)
    {
       while((file = readdir (rep)))
       {
+	 if(match == MAX_COMPLETION)
+	    break;
 	 if(!strncmp(p, file->d_name, strlen(p)))
+	 {
 	    q[match++] = file->d_name;
+	    type = file->d_type;
+	 }
       }
       if(match == 1)
       {
 	 match = strlen(p);
-	 p = malloc(sizeof(char) * (1+strlen(q[0]+match)));
+	 p = malloc(sizeof(char) * (2+strlen(q[0]+match)));
 	 strcpy(p, q[0]+match);
+	 if(type == DT_DIR)
+	    strcat(p, "/");
 	 closedir(rep);
 	 return p;
       }
-      else if(match > 1)
+      else if(match != 0)
       {
 	 for(j = 0; j < strlen(q[0])-strlen(p); j++)
 	 {
@@ -43,7 +73,7 @@ char *file_complete(char *str)
 	       if(q[i][j+strlen(p)] != q[0][j+strlen(p)])
 	       {
 		  match = strlen(p);
-		  p = malloc(sizeof(char) * (strlen(q[0]+match)-j+1));
+		  p = malloc(sizeof(char) * (strlen(q[0]+match)-j+2));
 		  strncpy(p, q[0]+match, j);
 		  p[j] = '\0';
 		  closedir(rep);
--- src/complete.h
+++ src/complete.h
@@ -3,5 +3,7 @@
 
 char *file_complete(char *str);
 
+/* au maximum 100 solutions de completions */
+#define MAX_COMPLETION 100
 
 #endif /* _COMPLETE_HEARDER */
