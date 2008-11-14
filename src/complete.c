f722ff97530053f9312b751dd0eedff65bf68b82
Philippe Pepiot <philippe.pepiot@gmail.com>
2008-11-14 14:40:40 +0100

    Completion si plusieurs matches

--- src/complete.c
+++ src/complete.c
@@ -4,16 +4,14 @@
 #include <dirent.h>
 #include "complete.h"
 
-/* Completion sur les fichiers
- * TODO : ecrire le debut si plusieurs
- * fichier marchent */
+/* Completion sur les fichiers */
 char *file_complete(char *str)
 {
    char *p = strrchr(str, ' ');
-   char *q;
+   char *q[100];
    DIR *rep;
    struct dirent *file;
-   size_t match = 0;
+   size_t match = 0, i, j;
    if(p == NULL)
       return NULL;
    while(*p == ' ')
@@ -26,18 +24,33 @@ char *file_complete(char *str)
       while((file = readdir (rep)))
       {
 	 if(!strncmp(p, file->d_name, strlen(p)))
-	 {
-	    match++;
-	    q = file->d_name;
-	 }
+	    q[match++] = file->d_name;
       }
       if(match == 1)
       {
 	 match = strlen(p);
-	 p = malloc(sizeof(char) * (1+strlen(q+match)));
-	 strcpy(p, q+match);
+	 p = malloc(sizeof(char) * (1+strlen(q[0]+match)));
+	 strcpy(p, q[0]+match);
 	 closedir(rep);
 	 return p;
+      }
+      else if(match > 1)
+      {
+	 for(j = 0; j < strlen(q[0])-strlen(p); j++)
+	 {
+	    for(i = 0; i < match; i++)
+	    {
+	       if(q[i][j+strlen(p)] != q[0][j+strlen(p)])
+	       {
+		  match = strlen(p);
+		  p = malloc(sizeof(char) * (strlen(q[0]+match)-j+1));
+		  strncpy(p, q[0]+match, j);
+		  p[j] = '\0';
+		  closedir(rep);
+		  return p;
+	       }
+	    }
+	 }
       }
       closedir(rep);
    }
